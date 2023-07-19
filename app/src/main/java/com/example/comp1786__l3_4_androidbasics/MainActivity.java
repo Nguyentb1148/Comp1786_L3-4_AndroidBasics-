84f6aeeb6ebe7b1397d3a93bccc8b23fd60d4a15
Nguyen Tran <nguyentran@Nguyens-MacBook-Pro-2.local>
2023-07-19 15:44:02 +0700

    Create menu(view)

--- app/src/main/java/com/example/comp1786__l3_4_androidbasics/MainActivity.java
+++ app/src/main/java/com/example/comp1786__l3_4_androidbasics/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.comp1786__l3_4_androidbasics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar myToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get a reference from the toolbar
+        myToolBar = findViewById(R.id.toolbar);
+
+// Set toolbar as actionbar for the activity
+        setSupportActionBar(myToolBar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if (item.getItemId() == R.id.itemNext){
+            getInputs();
+            return true;
+        }
+        else if (item.getItemId() == R.id.itemExit){
+            Toast.makeText(
+                    getApplicationContext(),
+                    "You asked to exit, but why not start another app?",
+                    Toast.LENGTH_LONG
+            ).show();
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void getInputs() {
     }
 }
